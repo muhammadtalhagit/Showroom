0423efc2e60773c9bc9788d44fd2ad6602049f5f
asp <asp@aptech.com>
2024-09-20 13:24:46 +0500

    Api done

---
+++ ShowroomApi/Controllers/CompanyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using ShowroomApi.Data;
+
+namespace ShowroomApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly ShowroomContext db;
+
+        public CompanyController(ShowroomContext _db)
+        {
+            db = _db;
+        }
+
+        [HttpGet]
+
+        public IActionResult GetManufactures()
+        {
+            return Ok(db.Manufacturers.ToList());
+        }
+    }
+}
